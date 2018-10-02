70f62a562d1579cdbc90fbb39ca9c7ab50b79910
dadapowell <jasonlalor@gmail.com>
2018-10-02 00:10:09 -0400

    fix redirect to https

--- app.js
+++ app.js
@@ -24,11 +24,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 require('./server/routes')(app);
 
 if (process.env.NODE_ENV === 'production') {
+    app.use (function (req, res, next) {
+        if (req.secure) {
+                // request was via https, so do no special handling
+                next();
+        } else {
+                // request was via http, so redirect to https
+                res.redirect('https://' + req.headers.host + req.url);
+        }
+    });
     // Serve any static files
-    app.use(express.static(path.join('https://', __dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     // Handle React routing, return all requests to React app
-    app.get('*', (req, res) => res.status(200).sendFile(path.join('https://', __dirname, 'client/build', 'index.html')));
+    app.get('*', (req, res) => res.status(200).sendFile(path.join(__dirname, 'client/build', 'index.html')));
 } else {
     app.get('*', (req, res) => res.status(200).send({message: "good to go"}));
 }
